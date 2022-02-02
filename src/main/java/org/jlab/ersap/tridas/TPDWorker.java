135b0a3d59a80c678f7e56a051a72d201994d280
gurjyan <gurjyan@jlab.org>
2022-02-02 18:06:05 -0500

    debugging ...

--- src/main/java/org/jlab/ersap/tridas/TPDWorker.java
+++ src/main/java/org/jlab/ersap/tridas/TPDWorker.java
@@ -11,25 +11,26 @@ package org.jlab.ersap.tridas;
  * @author gurjyan on 1/29/22
  * @project ersap-tridas
  */
+
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Event byte[] will have the following structure:
- *   uint32_t number of hits
- *   uint64_t StartTime in ns
- *   uint32_t TSCompleted flag for event belonging to an (un)completed (0)1 TS
- *
- *   unsigned int nseeds[6] array that store the number of seeds per trigger type
- *   unsigned int plugin_trigtype[8] array - seed-found flags for each kind of plugin
- *   unsigned int plugin_nseeds[8] array - n-seeds for each kind of L2 algorithm
- *   unsigned int Channel
- *   unsigned int Slot
- *   unsigned int Crate
- *   unsigned int Charge
- *   unsigned int T1ns 1 ns counter, 0-65536ns
- *   unsigned long long int VTP frame counter
+ * uint32_t number of hits
+ * uint64_t StartTime in ns
+ * uint32_t TSCompleted flag for event belonging to an (un)completed (0)1 TS
+ * <p>
+ * unsigned int nseeds[6] array that store the number of seeds per trigger type
+ * unsigned int plugin_trigtype[8] array - seed-found flags for each kind of plugin
+ * unsigned int plugin_nseeds[8] array - n-seeds for each kind of L2 algorithm
+ * unsigned int Channel
+ * unsigned int Slot
+ * unsigned int Crate
+ * unsigned int Charge
+ * unsigned int T1ns 1 ns counter, 0-65536ns
+ * unsigned long long int VTP frame counter
  */
 public class TPDWorker {
     private final static int TEventTag = 12081972;
@@ -40,32 +41,36 @@ public class TPDWorker {
 
         events.clear();
         System.out.println("========= DDD============");
-        System.out.println("Number of events = "+nEvents);
-//        for (int i = 0; i <= nEvents; i++) {
-//
-//            tSlice.getInt(); // pading
-//                    int magic = tSlice.getInt();
-//                    System.out.println("DDD =="+ String.format("magic = %x", magic) + " " + magic);
-//                    int evtId = tSlice.getInt();
-//                    System.out.println("DDD =="+ String.format("evtId = %x", evtId) + " " + evtId);
-//                    int evtLength = tSlice.getInt();
-//
-//            if (magic == TEventTag) {
-//                final byte[] dst = new byte[evtLength - 12];
-//                tSlice.get(dst);
-//                events.add(dst);
-//            }
-//        }
-//        currentIndex = events.size();
-        System.out.println("========= DDD============\n");
-    }
+        System.out.println("Number of events = " + nEvents);
+//        int magic = 0;
+//        do {
+//            magic = tSlice.getInt();
+//        } while (magic != TEventTag);
 
-    public byte[] getEvent() {
-        if (events.size() < currentIndex) {
-            currentIndex++;
-            return events.get(currentIndex);
-        } else {
-            return null;
+        for (int i = 0; i <= nEvents; i++) {
+            tSlice.getInt(); // pading
+            int magic = tSlice.getInt();
+            System.out.println("DDD ==" + String.format("magic = %x", magic) + " " + magic);
+            int evtId = tSlice.getInt();
+            System.out.println("DDD ==" + String.format("evtId = %x", evtId) + " " + evtId);
+            int evtLength = tSlice.getInt();
+
+            if (magic == TEventTag) {
+                final byte[] dst = new byte[evtLength - 16];
+                tSlice.get(dst);
+                events.add(dst);
+            }
+        }
+        currentIndex = events.size();
+            System.out.println("========= DDD============\n");
+        }
+
+        public byte[] getEvent () {
+            if (events.size() < currentIndex) {
+                currentIndex++;
+                return events.get(currentIndex);
+            } else {
+                return null;
+            }
         }
     }
-}
